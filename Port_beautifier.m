63190f940473658e5f90ec03ef47d6c34a6c7d15
MadhanRavi <31384693+madhanravi17@users.noreply.github.com>
2024-03-09 00:44:51 +0100

    Create Port_beautifier.m

---
+++ Port_beautifier.m
@@ -0,0 +1,28 @@
+%% Aligning ports (Ports Abstimmung) Inports
+h1 = get_param('Simulink_Programming/sd', 'PortConnectivity');
+for k = 1 : numel(h1)
+    if nnz(h1(k).SrcBlock)
+        n = get_param(h1(k).SrcBlock,'Name');
+        pos = get_param(['Simulink_Programming/', n], 'position');
+        HeighT = (-pos(2)+pos(4))/2;
+        WidtH = (-pos(1)+pos(3))/2;        
+        pos(1:2:4) = h1(k).Position(1) + [-WidtH-100, WidtH-100]; %100 spacing between port and subsytem
+        pos(2:2:4) = [h1(k).Position(2)-HeighT, h1(k).Position(2)+HeighT];
+        set_param(['Simulink_Programming/', n], 'Position',pos)
+    else continue
+    end
+end
+%%Aligning ports (Ports Abstimmung) Outports
+h1 = get_param('Simulink_Programming/sd', 'PortConnectivity');
+for k = 1 : numel(h1)
+    if nnz(h1(k).DstBlock)
+        n = get_param(h1(k).DstBlock,'Name');
+        pos = get_param(['Simulink_Programming/', n], 'position');
+        HeighT = (-pos(2)+pos(4))/2;
+        WidtH = (-pos(1)+pos(3))/2;        
+        pos(1:2:4) = h1(k).Position(1) + [-WidtH+100, WidtH+100]; %100 spacing between port and subsytem
+        pos(2:2:4) = [h1(k).Position(2)-HeighT, h1(k).Position(2)+HeighT];
+        set_param(['Simulink_Programming/', n], 'Position',pos)
+    else continue
+    end
+end
